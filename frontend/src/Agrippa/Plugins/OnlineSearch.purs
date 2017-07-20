fa2c2a9c35472e782c67e2ea5445df838dd8ad8b
Harry Pan <pan.xingzhi@gmail.com>
2017-07-19 22:00:57 -0700

    Better feedback for OnlineSearch.

--- frontend/src/Agrippa/Plugins/OnlineSearch.purs
+++ frontend/src/Agrippa/Plugins/OnlineSearch.purs
@@ -1,10 +1,10 @@
 module Agrippa.Plugins.OnlineSearch (search, prompt) where
 
-import Prelude (Unit, bind, otherwise, pure, (<>))
+import Prelude (Unit, bind, otherwise, pure, (==), (<<<), (<>))
 import Control.Monad.Eff (Eff)
-import Data.Array (filter, head, (:))
+import Data.Array (head, (:))
 import Data.Maybe (Maybe(..))
-import Data.StrMap (StrMap, fromFoldable, keys, lookup)
+import Data.StrMap (StrMap, filterKeys, fromFoldable, keys, size, values)
 import Data.String (Pattern(..), contains, joinWith, null, trim)
 import Data.Tuple (Tuple(..))
 import DOM (DOM)
@@ -14,35 +14,43 @@ import DOM.HTML.Window (open)
 
 prompt :: String -> String
 prompt input
-  | null (trim input) = ""
-  | otherwise = joinWith "\n"
-      ( "Matching websites (press <Enter> to goto the first one):"
-      : matchWebsites (trim input)
-      )
+  | null (trim input) = buildPromptString urlsByKey
+  | otherwise = (prompt' <<< matchWebsites <<< trim) input
+
+prompt' :: StrMap String -> String
+prompt' matched
+  | size matched == 1 =
+      case head (values matched) of
+        Nothing  -> "Something went really wrong..."
+        Just url ->  "Press <Enter> to search " <> url
+  | otherwise = buildPromptString matched
 
 search :: forall e. String
                  -> (String -> Eff (dom :: DOM, window :: WINDOW | e) Unit)
                  -> Eff (dom :: DOM, window :: WINDOW | e) String
 search input _ =
   let trimmedInput = trim input
-      websites = matchWebsites trimmedInput
-  in case (head websites) of
-    Just w  -> openWebsite w
-    Nothing -> pure ("No match found for: " <> trimmedInput <> ".")
+      matched = matchWebsites trimmedInput
+  in search' matched
+
+search' :: forall e. StrMap String -> Eff (dom :: DOM, window :: WINDOW | e) String
+search' matched
+  | size matched == 1 =
+      case head (values matched) of
+        Nothing  -> pure "Something went really wrong..."
+        Just url -> openWebsite url
+  | otherwise = pure (buildPromptString matched)
 
 openWebsite :: forall e. String -> Eff (dom :: DOM, window :: WINDOW | e) String
-openWebsite key =
-  case lookup key urlsByKey of
-    Just url -> do
-      w <- window
-      maybeNewWindow <- open url "_blank" "" w
-      pure case maybeNewWindow of
-            Nothing -> "Something went really wrong..."
-            Just _  -> "Opening a new window..."
-    Nothing -> pure ("No match found for: " <> key <> ".")
+openWebsite url = do
+  w <- window
+  maybeNewWindow <- open url "_blank" "" w
+  pure case maybeNewWindow of
+        Nothing -> "Something went really wrong..."
+        Just _  -> "Opening a new window..."
 
-matchWebsites :: String -> Array String
-matchWebsites input = filter (contains (Pattern input)) (keys urlsByKey)
+matchWebsites :: String -> StrMap String
+matchWebsites input = filterKeys (contains (Pattern input)) urlsByKey
 
 urlsByKey :: StrMap String
 urlsByKey = fromFoldable
@@ -52,4 +60,8 @@ urlsByKey = fromFoldable
   , Tuple "wiki"      "https://wikipedia.org/wiki/Special:Search/${q}"
   ]
 
+buildPromptString :: StrMap String -> String
+buildPromptString map = joinWith "\n" ("Keep typing until one is left:" : (keys map))
+
 -- TODO put urls in a config file
+-- TODO query using template
