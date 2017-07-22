34660c3e08d0548c68da5a3345088ca6934b928c
Harry Pan <pan.xingzhi@gmail.com>
2017-07-21 21:12:19 -0700

    Minor improvements.

--- frontend/src/Agrippa/Main.purs
+++ frontend/src/Agrippa/Main.purs
@@ -22,20 +22,21 @@ main :: forall e. Eff (ajax :: AJAX, dom :: DOM, window :: WINDOW | e) Unit
 main = ready do
   input <- select "#agrippa-input"
   on "keyup" handleInput input
+
   helpLink <- select "#agrippa-help-link"
-  helpDiv <- select "#agrippa-help-content"
-  buildHelpTextForPlugins helpDiv
-  on "click" (handleHelpLink helpDiv) helpLink
+  helpContent <- select "#agrippa-help-content"
+  buildHelpTextForPlugins helpContent
+  on "click" (handleHelpLink helpContent) helpLink
 
 -- input
 
 handleInput :: forall e. JQueryEvent
                       -> JQuery
                       -> Eff (ajax :: AJAX, dom :: DOM, window :: WINDOW | e) Unit
-handleInput event inputElem = do
+handleInput event inputField = do
   keyCode <- getWhich event
-  v <- getValue inputElem
-  for_ (runExcept (readString v)) (dispatchToPlugin keyCode)
+  wholeInput <- getValue inputField
+  for_ (runExcept (readString wholeInput)) (dispatchToPlugin keyCode)
 
 dispatchToPlugin :: forall e. Int
                            -> String
@@ -43,28 +44,28 @@ dispatchToPlugin :: forall e. Int
 dispatchToPlugin keyCode wholeInput =
   let maybePlugin :: Maybe (Tuple Plugin String)
       maybePlugin = do
-        i                                 <- indexOf (Pattern " ") wholeInput
-        { before: keyword, after: input } <- splitAt i wholeInput
-        plugin                            <- lookup keyword pluginsByKeyword
-        Just (Tuple plugin input)
+        i                                       <- indexOf (Pattern " ") wholeInput
+        { before: keyword, after: pluginInput } <- splitAt i wholeInput
+        plugin                                  <- lookup keyword pluginsByKeyword
+        Just (Tuple plugin pluginInput)
   in do
-    indicatorElem <- select "#agrippa-plugin-indicator"
+    indicator <- select "#agrippa-plugin-indicator"
     case maybePlugin of
-      Just (Tuple (Plugin { name: n, onIncrementalChange: inc, onActivation: act }) input) -> do
-        setText n indicatorElem
+      Just (Tuple (Plugin { name: n, onIncrementalChange: inc, onActivation: act }) pluginInput) -> do
+        setText n indicator
         case keyCode of
-          13 -> act input displayOnOutputDiv >>= displayOnOutputDiv -- activation
-          otherwise -> displayOnOutputDiv (inc input)               -- incremental
+          13 -> act pluginInput displayOutput >>= displayOutput -- activation
+          otherwise -> displayOutput (inc pluginInput)          -- incremental
       Nothing ->
         case keyCode of
-          13 -> openWebsite ("https://www.google.com/search?q=" <> wholeInput) >>= displayOnOutputDiv
-          otherwise -> setText "No plugin selected.  Press <Enter> to search on Google." indicatorElem *> clearOutputDiv
+          13 -> openWebsite ("https://www.google.com/search?q=" <> wholeInput) >>= displayOutput
+          otherwise -> setText "No plugin selected.  Press <Enter> to search on Google." indicator *> clearOutput
 
-displayOnOutputDiv :: forall e. String -> Eff (dom :: DOM | e) Unit
-displayOnOutputDiv r = select "#agrippa-output" >>= setText r
+displayOutput :: forall e. String -> Eff (dom :: DOM | e) Unit
+displayOutput r = select "#agrippa-output" >>= setText r
 
-clearOutputDiv :: forall e. Eff (dom :: DOM | e) Unit
-clearOutputDiv = select "#agrippa-output" >>= setText ""
+clearOutput :: forall e. Eff (dom :: DOM | e) Unit
+clearOutput = select "#agrippa-output" >>= setText ""
 
 pluginsByKeyword :: StrMap Plugin
 pluginsByKeyword = fromFoldable ((\p@(Plugin { keyword: k }) -> Tuple k p) <$> plugins)
@@ -72,7 +73,7 @@ pluginsByKeyword = fromFoldable ((\p@(Plugin { keyword: k }) -> Tuple k p) <$> p
 -- help
 
 buildHelpTextForPlugins :: forall e. JQuery -> Eff (dom :: DOM | e) Unit
-buildHelpTextForPlugins helpDiv = foldM buildHelpTextForPlugin unit plugins
+buildHelpTextForPlugins helpContent = foldM buildHelpTextForPlugin unit plugins
 
 buildHelpTextForPlugin :: forall e. Unit -> Plugin -> Eff (dom :: DOM | e) Unit
 buildHelpTextForPlugin _ (Plugin { name: n, keyword: key }) = do
@@ -83,8 +84,6 @@ buildHelpTextForPlugin _ (Plugin { name: n, keyword: key }) = do
     createTd :: String -> JQuery -> Eff (dom :: DOM | e) Unit
     createTd txt tr = create "<td>" >>= \td -> setText txt td *> append td tr
 
-handleHelpLink :: forall e. JQuery
-                         -> JQueryEvent
-                         -> JQuery
-                         -> Eff (dom :: DOM | e) Unit
-handleHelpLink helpDiv _ _ = toggle helpDiv
+handleHelpLink :: forall e. JQuery -> JQueryEvent -> JQuery -> Eff (dom :: DOM | e) Unit
+handleHelpLink helpContent _ _ = toggle helpContent
+
