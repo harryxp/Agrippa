54752700d643e309d78a9e9af87b359a14395b21
Harry Pan <pan.xingzhi@gmail.com>
2017-10-09 22:32:16 -0700

    Index using predicates.

--- server/src/Agrippa/Plugins/FileSystem/Index.hs
+++ server/src/Agrippa/Plugins/FileSystem/Index.hs
@@ -2,11 +2,12 @@
 module Agrippa.Plugins.FileSystem.Index (buildSearchIndices) where
 
 import Data.Aeson (Object, Value(Object))
-import Data.HashMap.Lazy (elems, member)
 import Data.List (intercalate)
-import System.Directory (createDirectoryIfMissing)
-import System.FilePath.Find (always, find)
+import System.Directory (createDirectoryIfMissing, renameFile)
+import System.FilePath.Find (FileType(Directory, RegularFile, SymbolicLink), FilterPredicate, RecursionPredicate, always, extension, find, fileType, (==?), (/=?), (&&?), (||?))
 import System.FilePath.Posix ((</>))
+
+import qualified Data.HashMap.Lazy as M (HashMap, elems, fromList, lookup, member)
 
 import Agrippa.Utils (getConfigDir, lookupJSON)
 
@@ -14,10 +15,10 @@ buildSearchIndices :: Object -> IO ()
 buildSearchIndices agrippaConfig =
   case lookupJSON "tasks" agrippaConfig :: Maybe Object of
     Nothing    -> error "Can't find tasks when building indices."
-    Just tasks -> (mapM_ buildSearchIndex . filter hasPaths . elems) tasks
+    Just tasks -> (mapM_ buildSearchIndex . filter hasPaths . M.elems) tasks
 
 hasPaths :: Value -> Bool
-hasPaths (Object o) = member "paths" o
+hasPaths (Object o) = M.member "paths" o
 hasPaths _          = False
 
 buildSearchIndex :: Value -> IO ()
@@ -38,6 +39,33 @@ buildSearchIndex' taskName plugin paths = do
   configDir <- getConfigDir
   let indexDir = configDir </> plugin </> taskName
   createDirectoryIfMissing True indexDir
-  files     <- (fmap concat . mapM (find always always)) paths :: IO [String]
-  writeFile (indexDir </> "index") (intercalate "\n" files)
+  let predicates = do
+       recursionPred <- M.lookup plugin recursionPredicatesByPlugin
+       filterPred    <- M.lookup plugin filterPredicatesByPlugin
+       return (recursionPred, filterPred)
+  files <- case predicates of
+    Just (rPred, fPred) -> (fmap concat . mapM (find rPred fPred)) paths :: IO [String]
+    Nothing -> error ("Failed to lookup predicates for plugin" ++ plugin)
+  writeFile (indexDir </> "index.tmp") (intercalate "\n" files)
+  renameFile (indexDir </> "index.tmp") (indexDir </> "index")
+
+recursionPredicatesByPlugin :: M.HashMap String RecursionPredicate
+recursionPredicatesByPlugin = M.fromList [ ("ExecutableSearch", always)
+                                       , ("LinuxFileSearch",  always)
+                                       , ("MacAppSearch",     (extension /=? ".app"))
+                                       , ("MacFileSearch",    always)
+                                       ]
+
+filterPredicatesByPlugin :: M.HashMap String FilterPredicate
+filterPredicatesByPlugin = M.fromList [ ("ExecutableSearch", isFile)
+                                    , ("LinuxFileSearch",  always)
+                                    , ("MacAppSearch",     isMacApp)
+                                    , ("MacFileSearch",    always)
+                                    ]
+
+isFile :: FilterPredicate
+isFile = fileType ==? RegularFile ||? fileType ==? SymbolicLink
+
+isMacApp :: FilterPredicate
+isMacApp = fileType ==? Directory &&? extension ==? ".app"
 
